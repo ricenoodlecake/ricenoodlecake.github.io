e88d57eb42a9125fef396078532cfb0e2b1dfc52
Justin Reyes <56141549+ricenoodlecake@users.noreply.github.com>
2021-09-17 02:54:21 -0700

    Commented Out Code Yet To Be Used 🎮

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const canvas = document.querySelector('canvas')
+const player = document.querySelector('.player')
 const gameCamera = document.getElementById('gameCamera')
 const ctx = canvas.getContext('2d')
 
 var keyPresses = []
 const playerSize = 25
 
-canvas.width = gameCamera.clientWidth
-canvas.height = gameCamera.clientHeight
+var screenWidth = window.screen.availWidth
+var screenHeight = window.screen.availHeight
 
-console.log(canvas.width)
+console.log(player)
+
+//test case
+var randX = []
+var randY = []
+for(var i = 0; i < 20; i++) {
+    randX[i] = 50+Math.floor(Math.random()*screenWidth-5)
+}
+for(var i = 0; i < 20; i++) {
+    randY[i] = 50+Math.floor(Math.random()*screenHeight-5)
+}
 
 class Map {
     constructor(x, y, wt, ht, color) {
@@ -20,7 +31,12 @@ class Map {
     }
     draw() {
         ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.wt, this.ht)
+        ctx.fillRect(this.x, this.y, this.wt*5, this.ht*5)
+
+        for(var i = 0; i < 20; i++) {
+            ctx.fillStyle = 'black'
+            ctx.fillRect(this.x+randX[i], this.y+randY[i], 25, 25)
+        }
     }
 }
 
@@ -40,47 +56,60 @@ class Player {
 
 function queueMovement(e) {
     if(e.keyCode == 38 || e.keyCode == 87) {       //up arrow key/W
-        keyPresses[2] = true
+        keyPresses[0] = true
     }
     if(e.keyCode == 37 || e.keyCode == 65) {       //left arrow key/A
         keyPresses[1] = true
     }
     if(e.keyCode == 40 || e.keyCode == 83) {       //down arrow key/S
-        keyPresses[3] = true
+        keyPresses[2] = true
     }
     if(e.keyCode == 39 || e.keyCode == 68) {       //right arrow key/D
-        keyPresses[0] = true
+        keyPresses[3] = true
     }
 }
 
 function resetMovement(e) {
     if(e.keyCode == 38 || e.keyCode == 87) {       //up arrow key/W
-        keyPresses[2] = false
+        keyPresses[0] = false
     }
     if(e.keyCode == 37 || e.keyCode == 65) {       //left arrow key/A
         keyPresses[1] = false
     }
     if(e.keyCode == 40 || e.keyCode == 83) {       //down arrow key/S
-        keyPresses[3] = false
+        keyPresses[2] = false
     }
     if(e.keyCode == 39 || e.keyCode == 68) {       //right arrow key/D
-        keyPresses[0] = false
+        keyPresses[3] = false
     }
 }
 
 function update() {
-    for(var i = 0; i < keyPresses.length; i++) {
-        if(keyPresses[i]) console.log("Moving")
+    if(keyPresses[0]) {
+        map.y += 5
+        map.draw()
+    }
+    if(keyPresses[1]) {
+        map.x += 5
+        map.draw()
+    }
+    if(keyPresses[2]) {
+        map.y -= 5
+        map.draw()
+    }
+    if(keyPresses[3]) {
+        map.x -= 5
+        map.draw()
     }
 }
 
-document.onkeydown = queueMovement
-document.onkeyup = resetMovement
+//document.onkeydown = queueMovement
+//document.onkeyup = resetMovement
 
 const map = new Map(0, 0, canvas.width, canvas.height, '#646877')
-map.draw()
+//map.draw()
 
-const player = new Player(canvas.width/2 - playerSize, canvas.height/2 - playerSize, playerSize, playerSize, 'white')
-player.draw()
+//const player = new Player(canvas.width/2 - playerSize, canvas.height/3 - playerSize, playerSize, playerSize, 'white')
+//player.draw()
 
 setInterval(update, 1000/100)
